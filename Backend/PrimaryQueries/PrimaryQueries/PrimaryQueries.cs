83861a3322464e1d040bb8f848ed1bb073f0b948
Tyler <tha7556@users.noreply.github.com>
2018-03-22 18:16:49 -0400

    Finished this disgusting web scraper

--- Backend/PrimaryQueries/PrimaryQueries/PrimaryQueries.cs
+++ Backend/PrimaryQueries/PrimaryQueries/PrimaryQueries.cs
@@ -68,7 +68,7 @@ namespace PrimaryQueries {
             return Query(query, false);
         }
         public static void PopulateTable() {
-            string current = "motherboard";
+            string current = "case";
             if (current.Equals("cpu")) {
                 string content = System.IO.File.ReadAllText("C://Users//Tyler//Desktop//cpu.html");
                 while (content.IndexOf("<tr>") != -1) {
@@ -92,7 +92,7 @@ namespace PrimaryQueries {
                     Console.WriteLine("price: " + price + "\n-----------------");
                 }
             }
-            else if(current.Equals("fan")) {
+            else if (current.Equals("fan")) {
                 string content = System.IO.File.ReadAllText("C://Users//Tyler//Desktop//fans.html");
                 while (content.IndexOf("<tr>") != -1) {
                     content = content.Substring(content.IndexOf("<tr>") + 2);
@@ -128,18 +128,164 @@ namespace PrimaryQueries {
                     string form = sub.Substring(0, sub.IndexOf("<"));
                     Console.WriteLine("form: " + form);
                     sub = sub.Substring(sub.IndexOf("<td>") + 4);
-                    sub = sub.Substring(sub.IndexOf(";")+3);
+                    sub = sub.Substring(sub.IndexOf(";") + 3);
                     string slots = sub.Substring(0, sub.IndexOf("<"));
                     Console.WriteLine("slots: " + slots);
                     sub = sub.Substring(sub.IndexOf(";") + 3);
-                    string maxRam = sub.Substring(0,sub.IndexOf("<"));
+                    string maxRam = sub.Substring(0, sub.IndexOf("<"));
                     Console.WriteLine("max ram: " + maxRam);
                     sub = sub.Substring(sub.IndexOf("price") + 8);
                     string price = sub.Substring(0, sub.IndexOf("<"));
                     Console.WriteLine("price: " + price + "\n-----------------");
-                    
                 }
             }
+            else if (current.Equals("memory")) {
+                string content = System.IO.File.ReadAllText("C://Users//Tyler//Desktop//memory.html");
+                while (content.IndexOf("<tr>") != -1) {
+                    content = content.Substring(content.IndexOf("<tr>") + 2);
+                    string sub = content.Substring(0, content.IndexOf("</tr>"));
+                    sub = sub.Substring(sub.IndexOf("a href"));
+                    sub = sub.Substring(sub.IndexOf(">") + 1);
+                    string name = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("name: " + name);
+                    sub = sub.Substring(sub.IndexOf(";") + 3);
+                    string speed = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("speed: " + speed);
+                    sub = sub.Substring(sub.IndexOf(";") + 3);
+                    string type = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("type: " + type);
+                    sub = sub.Substring(sub.IndexOf("<td>") + 4);
+                    sub = sub.Substring(sub.IndexOf(";") + 3);
+                    string cas = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("cas: " + cas);
+                    sub = sub.Substring(sub.IndexOf(";") + 3);
+                    string modules = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("modules: " + modules);
+                    sub = sub.Substring(sub.IndexOf(";") + 3);
+                    string size = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("size: " + size);
+                    sub = sub.Substring(sub.IndexOf("price") + 8);
+                    string price = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("price: " + price + "\n-----------------");
+                }
+            }
+            else if (current.Equals("storage")) {
+                string content = System.IO.File.ReadAllText("C://Users//Tyler//Desktop//storage.html");
+                while (content.IndexOf("<tr>") != -1) {
+                    content = content.Substring(content.IndexOf("<tr>") + 2);
+                    string sub = content.Substring(0, content.IndexOf("</tr>"));
+                    sub = sub.Substring(sub.IndexOf("a href"));
+                    sub = sub.Substring(sub.IndexOf(">") + 1);
+                    string name = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("name: " + name);
+                    sub = sub.Substring(sub.IndexOf("<td>") + 4);
+                    string series = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("series: " + series);
+                    sub = sub.Substring(sub.IndexOf(";") + 3);
+                    string form = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("form: " + form);
+                    sub = sub.Substring(sub.IndexOf("<td>") + 4);
+                    sub = sub.Substring(sub.IndexOf(";") + 3);
+                    string type = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("type: " + type);
+                    sub = sub.Substring(sub.IndexOf(";") + 3);
+                    string capacity = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("capacity: " + capacity);
+                    sub = sub.Substring(sub.IndexOf(";") + 3);
+                    string cache = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("cache: " + cache);
+                    sub = sub.Substring(sub.IndexOf("price") + 8);
+                    string price = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("price: " + price + "\n-----------------");
+                }
+            }
+            else if (current.Equals("graphicsCard")) {
+                string content = System.IO.File.ReadAllText("C://Users//Tyler//Desktop//graphicsCard.html");
+                while (content.IndexOf("<tr>") != -1) {
+                    content = content.Substring(content.IndexOf("<tr>") + 2);
+                    string sub = content.Substring(0, content.IndexOf("</tr>"));
+                    sub = sub.Substring(sub.IndexOf("a href"));
+                    sub = sub.Substring(sub.IndexOf(">") + 1);
+                    string name = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("name: " + name);
+                    sub = sub.Substring(sub.IndexOf("<td>") + 4);
+                    string series = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("series: " + series);
+                    sub = sub.Substring(sub.IndexOf(";") + 3);
+                    string chipset = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("chipset: " + chipset);
+                    sub = sub.Substring(sub.IndexOf("<td>") + 4);
+                    sub = sub.Substring(sub.IndexOf(";") + 3);
+                    string memory = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("memory: " + memory);
+                    sub = sub.Substring(sub.IndexOf(";") + 3);
+                    sub = sub.Substring(sub.IndexOf(";") + 3);
+                    string coreclock = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("coreclock: " + coreclock);
+                    sub = sub.Substring(sub.IndexOf("price") + 8);
+                    string price = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("price: " + price + "\n-----------------");
+                }
+            }
+            else if (current.Equals("power")) {
+                string content = System.IO.File.ReadAllText("C://Users//Tyler//Desktop//power.html");
+                while (content.IndexOf("<tr>") != -1) {
+                    content = content.Substring(content.IndexOf("<tr>") + 2);
+                    string sub = content.Substring(0, content.IndexOf("</tr>"));
+                    sub = sub.Substring(sub.IndexOf("a href"));
+                    sub = sub.Substring(sub.IndexOf(">") + 1);
+                    string name = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("name: " + name);
+                    sub = sub.Substring(sub.IndexOf("<td>") + 4);
+                    string series = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("series: " + series);
+                    sub = sub.Substring(sub.IndexOf(";") + 3);
+                    string form = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("form: " + form);
+                    sub = sub.Substring(sub.IndexOf("<td>") + 4);
+                    sub = sub.Substring(sub.IndexOf(";") + 3);
+                    string efficiency = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("efficiency: " + efficiency);
+                    sub = sub.Substring(sub.IndexOf(";") + 3);
+                    string watts = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("watts: " + watts);
+                    sub = sub.Substring(sub.IndexOf(";") + 3);
+                    string modular = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("modular: " + modular);
+                    sub = sub.Substring(sub.IndexOf("price") + 8);
+                    string price = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("price: " + price + "\n-----------------");
+                }
+            }
+            else if (current.Equals("case")) {
+                string content = System.IO.File.ReadAllText("C://Users//Tyler//Desktop//case.html");
+                while (content.IndexOf("<tr>") != -1) {
+                    content = content.Substring(content.IndexOf("<tr>") + 2);
+                    string sub = content.Substring(0, content.IndexOf("</tr>"));
+                    sub = sub.Substring(sub.IndexOf("a href"));
+                    sub = sub.Substring(sub.IndexOf(">") + 1);
+                    string name = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("name: " + name);
+                    sub = sub.Substring(sub.IndexOf("<td>") + 4);
+                    sub = sub.Substring(sub.IndexOf(";") + 3);
+                    string type = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("type: " + type);
+                    sub = sub.Substring(sub.IndexOf("<td>") + 4);
+                    sub = sub.Substring(sub.IndexOf(";") + 3);
+                    string ext514 = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("ext 5 1/4: " + ext514);
+                    sub = sub.Substring(sub.IndexOf(";") + 3);
+                    string in312 = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("in 3 1/2: " + in312);
+                    sub = sub.Substring(sub.IndexOf(";") + 3);
+                    string powersupply = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("power supply: " + powersupply);
+                    sub = sub.Substring(sub.IndexOf("price") + 8);
+                    string price = sub.Substring(0, sub.IndexOf("<"));
+                    Console.WriteLine("price: " + price + "\n-----------------");
+                }
+            }
+
         } 
         static void Main(string[] args) {
             PopulateTable();
