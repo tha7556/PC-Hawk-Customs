cd6ee54c2999a3cc5452f6ab4667e587e1250b61
Tyler <tha7556@users.noreply.github.com>
2018-03-27 11:24:21 -0400

    Imported Primary Queries

--- PCHawkVer3/Program.cs
+++ PCHawkVer3/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PrimaryQueries;
 
 namespace PCHawkVer3
 {
@@ -14,9 +15,12 @@ namespace PCHawkVer3
         [STAThread]
         static void Main()
         {
+            Queries.Log(Queries.LogLevel.DEBUG, "Start Program");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new SignInForm());
+            Queries.CloseLog(); //Must be called at end, flushes the fileStream
+            
         }
     }
 }
