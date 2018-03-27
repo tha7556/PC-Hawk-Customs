1ecdcc146f920fb7f6596890ad5f68ba7762c3b6
Tyler <tha7556@users.noreply.github.com>
2018-03-27 00:40:29 -0400

    Added alternate Constructor, and added AddToDatabase()

--- Backend/PrimaryQueries/PrimaryQueries/Order.cs
+++ Backend/PrimaryQueries/PrimaryQueries/Order.cs
@@ -25,6 +25,15 @@ namespace PrimaryQueries {
             this.customer = customer;
         }
         /// <summary>
+        /// Creates a new Order with -1 as Order Number. When added to the database, it automatically sets it to the next number
+        /// </summary>
+        /// <param name="part">The Part being ordered</param>
+        /// <param name="employee">The Employee involved in the Order</param>
+        /// <param name="customer">The Customer making the Order</param>
+        public Order(Part part, Employee employee, Customer customer) : this(-1,part,employee,customer){
+            
+        }
+        /// <summary>
         /// Gets the number identifying the Order
         /// </summary>
         /// <returns>The order number</returns>
@@ -52,6 +61,13 @@ namespace PrimaryQueries {
         public Customer GetCustomer() {
             return customer;
         }
+        public void AddToDatabase() {
+            string num = orderNumber.ToString();
+            if (orderNumber == -1)
+                num = "NULL";
+            PrimaryQueries.Query("INSERT INTO `order` (`order number`, `employee email`, `part number`, `customer email`) VALUES " +
+                "("+num+", '"+employee.GetEmail()+"', "+part.GetPartNumber()+", '"+customer.GetEmail()+"');");
+        }
         /// <summary>
         /// Converts a MySql query result into an Order
         /// </summary>
@@ -59,7 +75,7 @@ namespace PrimaryQueries {
         /// <returns>An Order made from the query</returns>
         public static Order GetOrderFromQuery(string result) {
             string[] line = result.Split('\0');
-            return new Order (int.Parse(line[0]),Part.GetPart(int.Parse(line[1])),Employee.GetEmployee(line[2]),Customer.GetCustomer(line[3]));
+            return new Order (int.Parse(line[0]),Part.GetPart(int.Parse(line[1])),Employee.Get(line[2]),Customer.Get(line[3]));
         }
     }
 }
