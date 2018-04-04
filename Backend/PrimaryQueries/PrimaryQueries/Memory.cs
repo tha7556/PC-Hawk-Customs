1362654d93ac9bb03cb18141163434d93bd339a4
d-dtm <dylan.menchetti@gmail.com>
2018-04-04 12:48:03 -0400

    Began implementation of Memory Part

---
+++ Backend/PrimaryQueries/PrimaryQueries/Memory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimaryQueries {
+    public class Memory : Part {
+        private string speed { get; set; }
+        private string type { get; set; }
+        private string cas { get; set; }
+    }
+}
