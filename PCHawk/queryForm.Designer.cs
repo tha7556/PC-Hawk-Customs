57c682b515fc08550c217c4d2f7b25c754eac607
d-dtm <dylan.menchetti@gmail.com>
2018-04-24 19:20:04 -0400

    buttonPush

--- PCHawk/queryForm.Designer.cs
+++ PCHawk/queryForm.Designer.cs
@@ -36,31 +36,35 @@
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // bttnTwoTable
             // 
-            this.bttnTwoTable.Location = new System.Drawing.Point(13, 159);
+            this.bttnTwoTable.Location = new System.Drawing.Point(16, 246);
+            this.bttnTwoTable.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.bttnTwoTable.Name = "bttnTwoTable";
-            this.bttnTwoTable.Size = new System.Drawing.Size(127, 75);
+            this.bttnTwoTable.Size = new System.Drawing.Size(169, 92);
             this.bttnTwoTable.TabIndex = 0;
             this.bttnTwoTable.Text = "Two Table Join";
             this.bttnTwoTable.UseVisualStyleBackColor = true;
             // 
             // bttnThreeTable
             // 
-            this.bttnThreeTable.Location = new System.Drawing.Point(12, 240);
+            this.bttnThreeTable.Location = new System.Drawing.Point(16, 346);
+            this.bttnThreeTable.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.bttnThreeTable.Name = "bttnThreeTable";
-            this.bttnThreeTable.Size = new System.Drawing.Size(127, 75);
+            this.bttnThreeTable.Size = new System.Drawing.Size(169, 92);
             this.bttnThreeTable.TabIndex = 1;
             this.bttnThreeTable.Text = "Three Table Join";
             this.bttnThreeTable.UseVisualStyleBackColor = true;
             // 
             // bttnExit
             // 
-            this.bttnExit.Location = new System.Drawing.Point(12, 42);
+            this.bttnExit.Location = new System.Drawing.Point(16, 52);
+            this.bttnExit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.bttnExit.Name = "bttnExit";
-            this.bttnExit.Size = new System.Drawing.Size(127, 64);
+            this.bttnExit.Size = new System.Drawing.Size(169, 79);
             this.bttnExit.TabIndex = 3;
             this.bttnExit.Text = "Close Page";
             this.bttnExit.UseVisualStyleBackColor = true;
@@ -68,9 +72,10 @@
             // 
             // displayBox
             // 
-            this.displayBox.Location = new System.Drawing.Point(145, 42);
+            this.displayBox.Location = new System.Drawing.Point(193, 52);
+            this.displayBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.displayBox.Name = "displayBox";
-            this.displayBox.Size = new System.Drawing.Size(553, 407);
+            this.displayBox.Size = new System.Drawing.Size(736, 500);
             this.displayBox.TabIndex = 4;
             this.displayBox.Text = "";
             // 
@@ -86,18 +91,20 @@
             "Motherboard",
             "Memory",
             "Storage"});
-            this.partBox.Location = new System.Drawing.Point(13, 375);
+            this.partBox.Location = new System.Drawing.Point(17, 462);
+            this.partBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.partBox.Name = "partBox";
-            this.partBox.Size = new System.Drawing.Size(121, 21);
+            this.partBox.Size = new System.Drawing.Size(160, 24);
             this.partBox.TabIndex = 5;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(12, 359);
+            this.label1.Location = new System.Drawing.Point(16, 442);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.Size = new System.Drawing.Size(143, 17);
             this.label1.TabIndex = 6;
             this.label1.Text = "Average Price of Part";
             // 
@@ -113,27 +120,40 @@
             "Motherboard",
             "Memory",
             "Storage"});
-            this.comboBox1.Location = new System.Drawing.Point(13, 415);
+            this.comboBox1.Location = new System.Drawing.Point(17, 511);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Size = new System.Drawing.Size(160, 24);
             this.comboBox1.TabIndex = 7;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(12, 399);
+            this.label2.Location = new System.Drawing.Point(16, 491);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.Size = new System.Drawing.Size(98, 17);
             this.label2.TabIndex = 8;
             this.label2.Text = "Part Requests";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 146);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 92);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Self-Join";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // queryForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(734, 461);
+            this.ClientSize = new System.Drawing.Size(979, 567);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
@@ -143,6 +163,7 @@
             this.Controls.Add(this.bttnThreeTable);
             this.Controls.Add(this.bttnTwoTable);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "queryForm";
             this.Text = "queryForm";
             this.ResumeLayout(false);
@@ -160,5 +181,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
